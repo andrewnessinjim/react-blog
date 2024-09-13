5708689c359f7adbdd1c7ed44ce3baf74552e98d
Andrew Nessin <andrewnessinjim@yahoo.com>
2024-09-13 21:40:04 +0530

    Exercise 4: Syntax highlighting

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -5,6 +5,7 @@ import BlogHero from '@/components/BlogHero';
 import styles from './postSlug.module.css';
 import { loadBlogPost } from '@/helpers/file-helpers';
 import { MDXRemote } from 'next-mdx-remote/rsc';
+import CodeSnippet from '@/components/CodeSnippet';
 
 export async function generateMetadata({params}) {
     const {frontmatter} = await loadBlogPost(params.postSlug);
@@ -24,7 +25,9 @@ async function BlogPost({params}) {
         publishedOn={frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote source={content}/>
+        <MDXRemote source={content} components={{
+          pre: CodeSnippet
+        }}/>
       </div>
     </article>
   );
