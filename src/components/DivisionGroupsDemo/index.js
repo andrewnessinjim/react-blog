1af1b944eedf97beac015a3cf5fc411d95432d11
Andrew Nessin <andrewnessinjim@yahoo.com>
2024-09-20 13:49:14 +0530

    Drop blog post from original template

--- src/components/DivisionGroupsDemo/index.js
+++
@@ -1,6 +0,0 @@
-'use client';
-
-import dynamic from "next/dynamic";
-
-const LazyDivisionGroupsDemo = dynamic(() => import("./DivisionGroupsDemo"));
-export default LazyDivisionGroupsDemo;
--- src/helpers/mdx-component.js
+++ src/helpers/mdx-component.js
@@ -1,6 +1,4 @@
-import CircularColorsDemo from '@/components/CircularColorsDemo';
 import CodeSnippet from '@/components/CodeSnippet';
-import DivisionGroupsDemo from '@/components/DivisionGroupsDemo';
 import IntersectionObserverVisualizer from '@/components/IntersectionObserverVisualizer';
 import { IndependentViewport } from '@/components/IntersectionObserverVisualizer/Viewport';
 import IndependentPage from '@/components/IntersectionObserverVisualizer/Page';
@@ -9,8 +7,6 @@ import SandpackDemo from '@/components/SandpackDemo';
 
 const COMPONENTS = {
     pre: CodeSnippet,
-    DivisionGroupsDemo,
-    CircularColorsDemo,
     SandpackDemo,
     IntersectionObserverVisualizer,
     IndependentPage,
