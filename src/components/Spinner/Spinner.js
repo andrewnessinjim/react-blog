e6f4dac9b1d7bf9aa6bbac308c8d9f8693274a95
Andrew Nessin <andrewnessinjim@yahoo.com>
2024-10-04 10:16:33 +0530

    Default settings for Spinner

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Loader } from 'react-feather';
 import { Wrapper } from './Spinner.styled';
 
-const Spinner = ({ color, size }) => {
+const Spinner = ({ color="inherit", size="24" }) => {
   return (
     <Wrapper >
       <Loader color={color} size={size} />
