cbb390d8c1362429717ff11d32f912b5a8cfcc4d
Andrew Nessin <andrewnessinjim@yahoo.com>
2024-10-10 19:26:29 +0530

    Animate blog header

--- src/components/BlogHero/BlogHero.js
+++ src/components/BlogHero/BlogHero.js
@@ -15,7 +15,21 @@ function BlogHero({ title, publishedOn = new Date(), ...delegated }) {
   return (
     <Wrapper {...delegated}>
       <Content>
-        <Heading>{title}</Heading>
+        <Heading
+          initial={{
+            y: -200,
+          }}
+          animate={{
+            y: 0,
+          }}
+          transition={{
+            type: "spring",
+            stiffness: 60,
+            damping: 15
+          }}
+        >
+          {title}
+        </Heading>
         <PublishedContainer>
           Published on{" "}
           <PublishedTime dateTime={publishedOn}>{humanizedDate}</PublishedTime>
